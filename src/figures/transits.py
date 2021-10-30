96d1e2cecd75f1d57ec06244a34605bf078493fb
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-10-29 21:22:26 -0400

    figure update

--- src/figures/transits.py
+++ src/figures/transits.py
@@ -37,230 +37,211 @@ plt.rcParams['legend.facecolor'] = 'none'
 edgecolor = '#05021f'
 k2_ec = '#b8b4b2'
 
-parula = ['#eb9c3b', '#74BB43', '#1A48A0', '#742C64']
-
-planets=['c','d','b','e']
-periods   = np.array([8.249147, 12.401369,  24.141445, 36.695032307689445])    
-t0s       = np.array([4648.53, 4645.4, 4648.1, 4648.8, 
-                      4645.4+periods[1], 4648.53+periods[0], 4648.53+periods[0]*2])
-durations = np.array([4.66, 5.59, 6.42, 7.45, 5.59, 4.66, 4.66])/24.0
-
 
 path = '../../data/'
 gp_mod = np.load(os.path.join(path, 'gp.npy'), allow_pickle=True).tolist()
 map_soln=np.load(os.path.join(path,'map_soln.npy'), allow_pickle=True).tolist()
 extras = np.load(os.path.join(path,'extras.npy'), allow_pickle=True).tolist()
 
+planets=['c','d','b','e']
+periods   = map_soln['period']
+t0s = map_soln['t0']
+t0s = np.append(t0s, [t0s[1]+periods[1], t0s[0]+periods[0], t0s[0]+periods[0]*2, 
+                      t0s[1]+periods[1]*2, t0s[2]+periods[2], t0s[0]+periods[0]*3])
+durations = np.array([4.66, 5.59, 6.42, 7.45, 5.59, 4.66, 4.66, 5.59, 6.42, 4.66])/24.0
+parula = ['#eb9c3b', '#74BB43', '#1A48A0', '#742C64', '#74BB43', '#eb9c3b',
+          '#eb9c3b', '#74BB43', '#1A48A0', '#eb9c3b']
+
+
+## The data
 time = gp_mod['time'] + 0.0
 flux = gp_mod['flux'] + 0.0
 flux_err = gp_mod['flux_err'] + 0.0
 model = gp_mod['gp_mod'] + 0.0
 planet_models = extras['light_curves_tess'] + 0.0
 
+rest = np.array([planet_models[:,1], planet_models[:,0], 
+                  planet_models[:,0], planet_models[:,1],
+                  planet_models[:,2], planet_models[:,0]]).T
+planet_models = np.append(planet_models, rest, axis=1)
 
-fig3 = plt.figure(constrained_layout=True, figsize=(16,20))
+####
+#The Figure
+####
+
+fig3 = plt.figure(constrained_layout=True, figsize=(20,26))
 fig3.set_facecolor('w')
-gs = fig3.add_gridspec(6,4, height_ratios=[2,1,1,2,1.5,1.5])
+gs = fig3.add_gridspec(4,4)
 
 bigger =20
 
+yflat = flux - model
+
 ## AX1
 ax1 = fig3.add_subplot(gs[0, :])
-ax1.set_title('TESS Light Curve for V1298 Tau', fontsize=26, y=1.2)
-ax1.set_ylabel('Normalized Flux', fontsize=bigger)
+ax1.set_title('Normalized & De-Trended TESS Light Curves for V1298 Tau', fontsize=26, y=1.3)
 
 ax1.errorbar(time, flux, yerr=flux_err,
              color='w', marker='o', linestyle='',
              markeredgecolor=edgecolor, zorder=1,
              ecolor=edgecolor)#, capsize=3)
 
-ax1.plot(time, model, lw=2, color='#0fa0cc', zorder=2,
+ax1.plot(time, model, lw=2.5, color='k', zorder=2,
          label='GP Model')
 ax1.set_xlim(time[0], time[-1])
+ax1.set_xticklabels([])
 
+divider = make_axes_locatable(ax1)
+rax = divider.append_axes("bottom", size='65%', pad=0)
 
-## AX2
-ax2 = fig3.add_subplot(gs[3, :])
-ax2.set_title('Detrended Light Curve', fontsize=bigger)
-ax2.set_ylabel('Normalized Flux', fontsize=bigger)
-ax2.set_xlabel('Time [BJD - 2457000]', fontsize=bigger)
+ax1.set_ylabel('Flux [ppt]', fontsize=25, y=0.2)
 
-yflat =  flux-model#-0.5
-ax2.errorbar(time, yflat, yerr=flux_err, 
+rax.errorbar(time, yflat, yerr=flux_err,
              color='w', marker='o', linestyle='',
-                 markeredgecolor=edgecolor, zorder=1,
-                 ecolor=edgecolor)#, capsize=3)
+             markeredgecolor=edgecolor, zorder=1,
+             ecolor=edgecolor)#, capsize=3)
 
-for i in range(4):
-    for j in range(3):
-        start = t0s[i]+(periods[i]*j)-durations[i]/2
-        stop = t0s[i]+(periods[i]*j)+durations[i]/2
-        ax2.axvspan(start, stop,
-                    ymin=flux.min(), ymax=flux.max(), 
+rax.set_xlim(time[0], time[-1])
+rax.set_xlabel('Time [BKJD - 2454833]', fontsize=25)
+
+
+for i in range(len(t0s)):
+    start = t0s[i]-durations[i]/2
+    stop = t0s[i]+durations[i]/2
+    if i < 4:
+        ax1.axvspan(start, stop,
+                    ymin=yflat.min(), ymax=yflat.max(), 
+                    color=parula[i], lw=0, alpha=0.5, linestyle='',
+                    label='Planet {}'.format(planets[i]))
+    else:
+        ax1.axvspan(start, stop,
+                    ymin=-10,ymax=4, 
                     color=parula[i], lw=0, alpha=0.5, linestyle='')
-        if j == 1:
-            ax1.axvspan(start, stop,
-                        ymin=yflat.min(), ymax=yflat.max(), 
-                        color=parula[i], lw=0, alpha=0.5, linestyle='',
-                        label='Planet {}'.format(planets[i]))
-        else:
-            ax1.axvspan(start, stop,
-                        ymin=yflat.min(), ymax=yflat.max(), 
-                        color=parula[i], lw=0, alpha=0.5, linestyle='')
+        rax.axvspan(start, stop,
+                    ymin=yflat.min(), ymax=yflat.max(), 
+                    color=parula[i], lw=0, alpha=0.5, linestyle='')
 
-ax1.legend(bbox_to_anchor=(0.08, 1.02, 1.3, .102), loc='lower left',
-           ncol=5, borderaxespad=0., fontsize=20, markerscale=6)#, mode="center", fontsize=20)
-
-ax2.set_xlim(time[0], time[-1])
+ax1.legend(bbox_to_anchor=(0.1, 1.02, 1.3, .102), 
+           loc='lower left',
+           ncol=5, borderaxespad=0., 
+           fontsize=20, markerscale=6)
+rax.set_ylim(-9, 3.5)
 
 transit_axes = []
 
-widths = [0, [1,4]]
-titles = ['Planet d', 'Planets b, c, & e']
-occurrence = [1, [2,0, 3]]
+widths = [0, [1,4], 
+          [0,3], 3, 
+          0, [1,4]]
+line = [1, 1, 
+        2, 2, 
+        3, 3]
+titles = ['Planet d', 'Planets b, c, & e', 
+          'Planet d & c', 'Planet c', 
+          'Planet d', 'Planets b & c']
+occurrence = [1, [2, 0, 3], 
+              [4, 5], 6, 
+              7, [8,9]]
 
 for i in range(len(widths)):
     if type(widths[i]) == list:
-        ax3 = fig3.add_subplot(gs[4, widths[i][0]:widths[i][1]])
-        ax5 = fig3.add_subplot(gs[1, widths[i][0]:widths[i][1]])
+        ax3 = fig3.add_subplot(gs[line[i], widths[i][0]:widths[i][1]])
     else:
-        ax3 = fig3.add_subplot(gs[4, widths[i]])
-        ax5 = fig3.add_subplot(gs[1, widths[i]])
-    ax3.set_title(titles[i], fontsize=bigger)
-    ax5.set_title(titles[i], fontsize=bigger)
+        ax3 = fig3.add_subplot(gs[line[i], widths[i]])
+    divider = make_axes_locatable(ax3)
+    rax3 = divider.append_axes("bottom", size='65%', pad=0)
+        
+    ax3.set_title(titles[i], fontsize=25)
     
     if type(occurrence[i]) == list:
         q = ( (time > t0s[occurrence[i][0]] - 0.5) &
               (time < t0s[occurrence[i][-1]] + 0.5) )
-        
+        if i == 2:
+            q = ( (time > t0s[occurrence[i][-1]] - 0.5) &
+                  (time < t0s[occurrence[i][0]] + 0.5) )
+
         for j in occurrence[i]:
-            ax3.plot(time, planet_models[:,j],
-                     c=parula[j], lw=6, zorder=3)
+            
+            if i == 1 and j == 0:
+                rax3.plot(time, 
+                          np.nansum(planet_models[:,occurrence[i]], axis=1),
+                         c='k', lw=4, zorder=3)
+            rax3.plot(time, planet_models[:,j],
+                      c=parula[j], lw=4, zorder=5)
+            
             start = t0s[j]-durations[j]/2
             stop = t0s[j]+durations[j]/2
 
-            ax5.axvspan(start, stop,
+            ax3.axvspan(start, stop,
                         ymin=flux.min(), ymax=flux.max(), 
                         color=parula[j], lw=0, alpha=0.5, linestyle='')
     else:
         q = ( (time > t0s[occurrence[i]] - 0.5) &
               (time < t0s[occurrence[i]] + 0.5) )
-
-        ax3.plot(time, planet_models[:,occurrence[i]],
-             c=parula[occurrence[i]], lw=lw, zorder=3)
-        
         start = t0s[occurrence[i]]-durations[occurrence[i]]/2
         stop = t0s[occurrence[i]]+durations[occurrence[i]]/2
 
-        ax5.axvspan(start, stop,
+        ax3.axvspan(start, stop,
                     ymin=flux.min(), ymax=flux.max(), 
                     color=parula[occurrence[i]], lw=0, alpha=0.5, linestyle='')
+        rax3.plot(time, planet_models[:,occurrence[i]],
+                 c=parula[occurrence[i]], lw=4, zorder=5)
     
-    ax3.errorbar(time[q], yflat[q], 
+    ax3.errorbar(time[q], flux[q], 
                  yerr=flux_err[q],
                  color='w', marker='o', linestyle='',
-                 markeredgecolor=edgecolor, zorder=1,
+                 markeredgecolor=edgecolor, zorder=3,
                  ecolor=edgecolor)
-    ax5.errorbar(time[q], flux[q], 
-                 yerr=flux_err[q],
-                 color='w', marker='o', linestyle='',
-                 markeredgecolor=edgecolor, zorder=1,
-                 ecolor=edgecolor)
-    ax5.plot(time[q], model[q], c='#0fa0cc', lw=3)
+    rax3.errorbar(time[q], yflat[q], 
+                  yerr=flux_err[q],
+                  color='w', marker='o', linestyle='',
+                  markeredgecolor=edgecolor, zorder=3,
+                  ecolor=edgecolor)
+    ax3.plot(time[q], model[q], c='k', lw=3, zorder=4)
     
+    yticks = np.round([np.nanmin(flux[q]),  
+                       (np.nanmin(flux[q]) +np.nanmean(flux[q]))/2.0,
+                       np.nanmax(flux[q])], 1)
+    ax3.set_yticks(yticks)
     
-    if i == 0:
-        ax3.set_ylabel('De-trended flux [ppt]', fontsize=bigger)
-        ticks = np.round([time[q][1],  time[q][int(len(time[q])/2)],
-                          time[q][-2]],2)
+    yticks = np.round([np.nanmin(yflat[q]),  
+                       (np.nanmin(yflat[q]) +np.nanpercentile(yflat[q], 95))/2.0,
+                       np.nanpercentile(yflat[q], 95)], 1)
+    rax3.set_yticks(yticks)
+    rax3.set_ylim(np.nanmin(yflat[q]), np.nanpercentile(yflat[q],99))
+    
+    if i == 0 or i ==4 or i == 3:
+        if i != 3:
+            ax3.set_ylabel('Flux [ppt]', fontsize=25, y=0.02)
+            
+        ticks = np.round([time[q][5],  
+                          time[q][int(len(time[q])/2)],
+                          time[q][-5]],2)
+
         ax3.set_xticks(ticks)
         ax3.set_xticklabels([str(e) for e in ticks])
-        ax5.set_xticks(ticks)
-        ax5.set_xticklabels([str(e) for e in ticks])
-
-    ax3.set_xlim(time[q][0], 
-                 time[q][-1])
-    ax5.set_xlim(time[q][0], 
-                 time[q][-1])
-    transit_axes.append(ax3)
-    transit_axes.append(ax5)
-    
-    
-ax4 = [fig3.add_subplot(gs[5, 0:3]), fig3.add_subplot(gs[5, 3])]
-ax6 = [fig3.add_subplot(gs[2, 0:3]), fig3.add_subplot(gs[2, 3])]
-titles = ['Planets d & c', 'Planet c']
-occurrence = [[5,4], 6]
-
-for i in range(2):
-    ax4[i].set_title(titles[i], fontsize=bigger)
-    ax6[i].set_title(titles[i], fontsize=bigger)
-    
-    if i == 0:
-        ax4[i].set_ylabel('De-trended flux [ppt]')#, fontsize=bigger)
-        ax6[i].set_ylabel('Normalized flux [ppt]', y=1.2)#, fontsize=bigger, y=1.2)
-        
-    if type(occurrence[i]) == list:
-        q = ( (time > t0s[occurrence[i][0]] - 0.5) &
-              (time < t0s[occurrence[i][-1]] + 0.5) )
-        
-        for j in occurrence[i]:
-            ax4[i].plot(time, planet_models[:,j-4],
-                        c=parula[j-4], lw=6, zorder=3)
-            start = t0s[j-4]+periods[j-4]-durations[j-4]/2
-            stop = t0s[j-4]+periods[j-4]+durations[j-4]/2
-
-            ax6[i].axvspan(start, stop,
-                        ymin=flux.min(), ymax=flux.max(), 
-                        color=parula[j-4], lw=0, alpha=0.5, linestyle='')
-            
+        rax3.set_xticks(ticks)
+        rax3.set_xticklabels([str(e) for e in ticks])
+        ax3.set_xlim(time[q][0], time[q][-1])
+        rax3.set_xlim(time[q][0], time[q][-1])
     else:
-        q = ( (time > t0s[occurrence[i]] - 0.5) &
-              (time < t0s[occurrence[i]] + 0.5) )
+        ax3.set_xlim(time[q][5], time[q][-5])
+        rax3.set_xlim(time[q][5], time[q][-5])
+        
+    if i == 2:
+        ax3.set_ylabel('Flux [ppt]', fontsize=25, y=0.2)
+    if i > 3:
+        rax3.set_xlabel('Time [BKJD - 2454833]', fontsize=25)
 
-        ax4[i].plot(time, planet_models[:,occurrence[i]-6],
-                    c=parula[occurrence[i]-6], lw=lw, zorder=3)
-        
-        start = t0s[occurrence[i]-6]+periods[occurrence[i]-6]*2-durations[occurrence[i]-6]/2
-        stop = t0s[occurrence[i]-6]+periods[occurrence[i]-6]*2+durations[occurrence[i]-6]/2
+    
+    transit_axes.append(ax3)
+    transit_axes.append(rax3)
+    ax3.set_xticklabels([])
+    
 
-        ax6[i].axvspan(start, stop,
-                        ymin=flux.min(), ymax=flux.max(), 
-                        color=parula[occurrence[i]-6], lw=0, alpha=0.5, linestyle='')
-        
-    ax4[i].errorbar(time[q], yflat[q], 
-                     yerr=flux_err[q],
-                     color='w', marker='o', linestyle='',
-                     markeredgecolor=edgecolor, zorder=1,
-                     ecolor=edgecolor)#, capsize=3)
-    ax6[i].errorbar(time[q], flux[q], 
-                     yerr=flux_err[q],
-                     color='w', marker='o', linestyle='',
-                     markeredgecolor=edgecolor, zorder=1,
-                     ecolor=edgecolor)#, capsize=3)
-    ax6[i].plot(time[q], model[q], c='#0fa0cc', lw=3)
-        
-    ax4[i].set_xlabel('Time [BJD - 2457000]')
-    ax4[i].set_xlim(time[q][0], 
-                        time[q][-1])
-    ax6[i].set_xlabel('Time [BJD - 2457000]')
-    ax6[i].set_xlim(time[q][0], 
-                        time[q][-1])
-    
-    if i == 1:
-        ax4[i].set_xticks(np.round([time[q][1], 
-                                    time[q][int(len(time[q])/2)],
-                                    time[q][-1]],1))
-        ax6[i].set_xticks(np.round([time[q][1], 
-                                    time[q][int(len(time[q])/2)],
-                                    time[q][-1]],1))
-    transit_axes.append(ax4[i])
-    transit_axes.append(ax6[i])
-    
 all_axes = transit_axes
 all_axes.append(ax1)
-all_axes.append(ax2)
 for ax in all_axes:
     ax.set_rasterized(True)
+plt.subplots_adjust(hspace=0.3, wspace=0.3)
     
 plt.savefig('transits.pdf', rasterize=True, bbox_inches='tight', dpi=300)
-      
